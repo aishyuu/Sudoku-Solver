60b4833d608acb1306ad4dd9d8418db2bcf9c512
aishyuu <ivanmatias1331@gmail.com>
2022-12-06 03:26:32 -0800

    Finished backtrack implementation (before testing)

--- terminal_solver.py
+++ terminal_solver.py
@@ -14,6 +14,47 @@ def main():
     print_board(board)
     print(find_empty(board))
 
+def solve(board):
+    #Seach for the coords of the empty
+    find = find_empty(board)
+    if not find:
+        #If you can't find an empty space, you're done
+        return True
+    else:
+        #Break the empty space into its row and column
+        row, column = find
+
+    #Plug in 1-9 one a a time into the empty space
+    for i in range(1,10):
+        # Validate the position with the current number (i)
+        if validate(board, i, row, column):
+            #If validated,
+            board[row][column] = i
+
+            if solve(board):
+                return True
+            
+            board[row][column] = 0
+
+def validate(board, num, pos_row, pos_col):
+    for i in range(len(board[0])):
+        if board[pos_row][i] == num and pos_col != i:
+            return False
+
+    for i in range(len(board)):
+        if board[i][pos_col] == num and pos_row != i:
+            return False
+
+    box_x = pos_col // 3
+    box_y = pos_row // 3
+
+    for i in range(box_y*3, box_y*3 + 3):
+        for j in range(box_x*3, box_x*3 + 3):
+            if board[i][j] == num and (i,j) != (pos_col, pos_row):
+                return False
+    
+    return True
+
 def print_board(board):
     """
     print the current sudoku board
